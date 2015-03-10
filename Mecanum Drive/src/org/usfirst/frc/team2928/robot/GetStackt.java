9471fb055e50f1a1b5206c0cfbe582e7a8b45c77
HappyFox <keith.baston@gmail.com>
2015-03-10 09:24:36 -0700

    Bit more of a cleanup of GetStackt.java. Also replacing the 0.5 and 0.75 with constants.

--- Mecanum Drive/src/org/usfirst/frc/team2928/robot/GetStackt.java
+++ Mecanum Drive/src/org/usfirst/frc/team2928/robot/GetStackt.java
@@ -12,38 +12,44 @@ import edu.wpi.first.wpilibj.TalonSRX;
 import edu.wpi.first.wpilibj.Timer;
 import edu.wpi.first.wpilibj.CANTalon.FeedbackDevice;
 
-public class GetStackt {
-	 int level=1;
-	 Solenoid s2;
-	 Solenoid s3;
-	 Solenoid s6;
-	 Solenoid s7;
-	 CANTalon leftAnalog;
-	 CANTalon rightAnalog;
-	 CANTalon t1;
-	 CANTalon t2;
-	 boolean  toggled = false;
-	 
-	 private static final int LEFT_ANAL_1 =574;
-	 private static final int LEFT_ANAL_2 = 555;
-	 private static final int LEFT_ANAL_3 = 362;
-	 private static final int LEFT_ANAL_4 = 473;
-	 int RIGHT_ANAL_1 = 578;
-	 int RIGHT_ANAL_2 = 555;
-	 int RIGHT_ANAL_3 = 370;
-	 int RIGHT_ANAL_4 = 478;
+public class GetStackt 
+{
+	boolean  toggled = false;
+	int level=1;
+    
+	Solenoid s2;
+	Solenoid s3;
+	Solenoid s6;
+	Solenoid s7;
+
+	CANTalon leftAnalog;
+	CANTalon rightAnalog;
+	CANTalon t1;
+	CANTalon t2;
+	
+	private static final int LEFT_ANAL_1 =574;
+	private static final int LEFT_ANAL_2 = 555;
+	private static final int LEFT_ANAL_3 = 362;
+	private static final int LEFT_ANAL_4 = 473;
+
+	private static final int RIGHT_ANAL_1 = 578;
+	private static final int RIGHT_ANAL_2 = 555;
+	private static final int RIGHT_ANAL_3 = 370;
+	private static final int RIGHT_ANAL_4 = 478;
+     
+    private static final double SPEED_SLOW = 0.5;
+    private static final double SPEED_FAST = 0.75;
 	 
 	JoystickButton b1;
 	JoystickButton b2;
-		
 	JoystickButton b3;
 	JoystickButton b4;
 	JoystickButton b5;
 	JoystickButton b6;
+
+
 	public GetStackt(CANTalon t1, CANTalon t2, Solenoid s2, Solenoid s3, Solenoid s6, Solenoid s7, CANTalon leftAnalog, CANTalon rightAnalog)
 	{
-		//t1.setFeedbackDevice(AnalogPot());
-	
 		this.s2 = s2;
 		this.s3 = s3;
 		this.s6 = s6;
@@ -54,11 +60,11 @@ public class GetStackt {
 		this.t2=t2;
 	}
 	
+
 	public void Stack(Joystick otherStick, Joystick stick, int num1, int num2, int num3, int num4, int num5, int num6)
 	{
 		this.b1 = new JoystickButton(otherStick,num1);
 		this.b2 = new JoystickButton(otherStick,num2);
-		
 		this.b3 = new JoystickButton(otherStick, num3);
 		this.b4 = new JoystickButton(otherStick, num4);
 		this.b5 = new JoystickButton(otherStick, num5);
@@ -70,6 +76,7 @@ public class GetStackt {
 			t1.set(-.25);
 			return;
 		}
+
 		t2.set(0);
 		t1.set(0);
 		
@@ -79,186 +86,110 @@ public class GetStackt {
 			t1.set(.25);
 			return;
 		}
+
 		t2.set(0);
 		t1.set(0);
-		
 	}
-	
+
 
 	public void ComparePos(CANTalon t2, CANTalon t1, CANTalon pos, int presetPos)
 	{
 		int currentPos = pos.getAnalogInPosition();
 		double speed;
+
 		if(toggled)
 		{
-			speed = 0.5;
+			speed = SPEED_SLOW;
 		}
 		else
 		{
-			speed = 0.75;
+			speed = SPEED_FAST;
 		}
-		if(currentPos <presetPos )
+
+		if(currentPos <presetPos)
 		{
 			speed = -speed;
-			
 		}
+
 		t2.set(speed);
 		t1.set(speed);
 	}
 	
+
 	public void Level1()
 	{		
 		this.ComparePos(t2, t1, this.leftAnalog, LEFT_ANAL_1);
 		this.ComparePos(t2, t1, this.rightAnalog, RIGHT_ANAL_1);
-		
 	}
 			
-		public void Level2()
-		{
-			this.ComparePos(t2, t1, this.leftAnalog, LEFT_ANAL_2);
-			this.ComparePos(t2, t1, this.rightAnalog, RIGHT_ANAL_2);
-				
-		}
-		
-		public void Level3()
-		{
-			this.ComparePos(t2, t1, this.leftAnalog, LEFT_ANAL_3);
-			this.ComparePos(t2, t1, this.rightAnalog, RIGHT_ANAL_3);
-		}
-		
-		
-		public void Level4()
-		{
-			this.ComparePos(t2, t1, this.leftAnalog, LEFT_ANAL_4);
-			this.ComparePos(t2, t1, this.rightAnalog, RIGHT_ANAL_4);
-		}			
 
-		
-		public void clamp()
-		{
-			toggled = true;
-			s3.set(true);
-			s2.set(false);
-			s6.set(false);
-			s7.set(true);
-			
-		}
-		
-		
-		public void unclamp()
-		{ 
-			toggled = false;
-			s3.set(false);
-			s2.set(true);
-			s6.set(true);
-			s7.set(false);	
-		}
-		
-		
-		public void up()
-		{
-			if(toggled)
-			{
-			t1.set(.5);
-			t2.set(.5);
-			}
-			else{
-			t1.set(.75);
-			t2.set(.75);
-			}
-		}
-		
-		
-		public void down()
-		{
-			if(toggled)
-			{
-				t1.set(-.5);
-				t2.set(-.5);
-			}
-			else
-			{
-				t1.set(-.75);
-				t2.set(-.75);
-			}
-		}
+    public void Level2()
+    {
+        this.ComparePos(t2, t1, this.leftAnalog, LEFT_ANAL_2);
+        this.ComparePos(t2, t1, this.rightAnalog, RIGHT_ANAL_2);
+    }
+    
 
+    public void Level3()
+    {
+        this.ComparePos(t2, t1, this.leftAnalog, LEFT_ANAL_3);
+        this.ComparePos(t2, t1, this.rightAnalog, RIGHT_ANAL_3);
+    }
+    
+    
+    public void Level4()
+    {
+        this.ComparePos(t2, t1, this.leftAnalog, LEFT_ANAL_4);
+        this.ComparePos(t2, t1, this.rightAnalog, RIGHT_ANAL_4);
+    }			
+
+    
+    public void clamp()
+    {
+        toggled = true;
+        s3.set(true);
+        s2.set(false);
+        s6.set(false);
+        s7.set(true);
+    }
+    
+    
+    public void unclamp()
+    { 
+        toggled = false;
+        s3.set(false);
+        s2.set(true);
+        s6.set(true);
+        s7.set(false);	
+    }
+    
+    
+    public void up()
+    {
+        if(toggled)
+        {
+            t1.set(.5);
+            t2.set(.5);
+        }
+        else
+        {
+            t1.set(.75);
+            t2.set(.75);
+        }
+    }
+    
+    
+    public void down()
+    {
+        if(toggled)
+        {
+            t1.set(-.5);
+            t2.set(-.5);
+        }
+        else
+        {
+            t1.set(-.75);
+            t2.set(-.75);
+        }
+    }
 }
-		
-	
-	
-
-/*	
-	while(level == 1 )
-		{
-			while(b5.get())
-			{
-				//Toggle
-				s3.set(!s3.get());
-				s2.set(!s2.get());
-				s6.set(!s6.get());
-				s7.set(!s7.get());
-				return;
-			}
-			return;
-		
-			
-
-		}
-	while(level == 2)
-	{
-		s3.set(true);
-		s2.set(false);
-		s6.set(false);
-		s7.set(true);
-		return;
-	}
-		while(level == 3 || level == 4)
-		{
-			while(b5.get())
-			{
-				//Toggle 
-				s3.set(!s3.get());
-				s2.set(!s2.get());
-				s6.set(!s6.get());
-				s7.set(!s7.get());
-				return;
-			}
-	
-			return;
-		
-		}
-		
-		
-		if(b1.get())
-		{
-			level = 1;
-			
-			s3.set(false);
-			s2.set(true);
-			s6.set(true);
-			s7.set(false);
-		}
-		
-		if(b2.get())
-		{
-			level = 2;
-		}
-		
-		if(b3.get())
-		{
-			level =3;
-			s3.set(true);
-			s2.set(false);
-			s6.set(false);
-			s7.set(true);
-		}
-		
-		if(b4.get())
-		{
-			level =4;
-			s3.set(true);
-			s2.set(false);
-			s6.set(false);
-			s7.set(true);
-		}*/
